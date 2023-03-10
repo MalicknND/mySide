73de3c7fe54910c892d00749e40e3f54aa17db28
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-03-10 14:15:30 +0000

    save👍

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let menu = document.querySelector (".menu-icon");
-let navbar = document.querySelector (".navbar");
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
   navbar.classList.toggle("open-menu");
@@ -12,68 +12,67 @@ window.onscroll = () => {
 
 // Reviews Swiper
 var swiper = new Swiper(".reviews-content", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// Email JS
+function validate() {
+  let name = document.querySelector(".name");
+  let email = document.querySelector(".email");
+  let msg = document.querySelector(".message");
+  let sendBtn = document.querySelector(".send-btn");
+
+  sendBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (name.value == "" || email.value == "" || msg.value == "") {
+      emptyerror();
+    } else {
+      sendmail(name.value, email.value, msg.value);
+      success();
+    }
   });
+}
+validate();
 
-  // Email JS
-  function validate() {
-    let name = document.querySelector(".name");
-    let email = document.querySelector(".email");
-    let msg = document.querySelector(".message");
-    let sendBtn = document.querySelector(".send-btn");
-
-    sendBtn.addEventListener("click", (e) => {
-      e.preventDefault();
-      if (name.value == "" || email.value == "" || msg.value == "") {
-        emptyerror();
-      } else {
-        sendmail(name.value, email.value, msg.value);
-        success();
-      }
-    });
-  }
-  validate();
-
-  function sendmail(name,email,msg){
-    emailjs.send("service_9cu0puf","template_ybig74m",{
-      from_name: email,
-      to_name: name,
-      message: msg,
-      });
-  }
-  function emptyerror() {
-    swal({
-      title: "Oh No...",
-      text: "Fields cannot be empty!",
-      icon: "error",
-    });
-  }
-  function success() {
-    swal({
-      title: "Email sent successfully",
-      text: "We try to reply in 24 hours",
-      icon: "success",
-    });
-  }
+function sendmail(name, email, msg) {
+  emailjs.send("service_9cu0puf", "template_ybig74m", {
+    from_name: email,
+    to_name: name,
+    message: msg,
+  });
+}
+function emptyerror() {
+  swal({
+    title: "Oh No...",
+    text: "Fields cannot be empty!",
+    icon: "error",
+  });
+}
+function success() {
+  swal({
+    title: "Email sent successfully",
+    text: "We try to reply in 24 hours",
+    icon: "success",
+  });
+}
 // Header Background change On Scroll
-let header = document.querySelecter("header");
+let header = document.querySelector("header");
 
-
-Window.addEventListener("scroll", () => {
+window.addEventListener("scroll", () => {
   header.classList.toggle("header-active", window.scrollY > 0);
 });
 
 //Scroll Top
-let scrollTop = document.querySelecter(".scroll-top");
+let scrollTop = document.querySelector(".scroll-top");
 
 window.addEventListener("scroll", () => {
   scrollTop.classList.toggle("scroll-active", window.scrollY >= 400);
